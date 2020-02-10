0d272d15eece45719d2c2e1f88abc51d97bfed63
Ozgun Ayaz <11738322+ozgunawesome@users.noreply.github.com>
2020-02-10 18:55:02 +0300

    refactored test method into a helper method

--- src/test/java/services/ozzy/dukeout/ApplicationTest.java
+++ src/test/java/services/ozzy/dukeout/ApplicationTest.java
@@ -4,9 +4,14 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
+import java.util.function.BiConsumer;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 /**
@@ -16,35 +21,51 @@ import static junit.framework.TestCase.assertTrue;
  */
 public class ApplicationTest {
 
-    @Test
-    public void runApplication() {
-        PrintStream out = System.out;
-        String testResults = null;
+    /**
+     * Helper method to test application entry point.
+     *
+     * @param evaluator         lambda to evaluate the application output
+     * @param argumentsToSearch arguments to provide to the application entry point
+     */
+    public void testApplicationWithArguments(BiConsumer<String, String> evaluator, String... argumentsToSearch) {
+        PrintStream defaultStdout = System.out, defaultStderr = System.err;
+        String testStdout = null, testStderr = null;
 
         try {
-            Logger.getLogger(this.getClass().getName()).info("Hooking STDOUT to check results, please wait..");
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            System.setOut(new PrintStream(byteArrayOutputStream));
-            Application.main("java", ".net");
+            Logger.getLogger(this.getClass().getName()).info("Hooking STDOUT and STDERR to check results, please wait..");
+            ByteArrayOutputStream stdoutStream = new ByteArrayOutputStream(), stderrStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(stdoutStream));
+            System.setErr(new PrintStream(stderrStream));
 
-            testResults = byteArrayOutputStream.toString();
-            // apparently ".net" will always win since Google will ignore the dot and crush it with 10x results
-            assertTrue(testResults.contains("Total winner: .net"));
+            Application.main(argumentsToSearch);
+
+            testStdout = stdoutStream.toString().trim();
+            testStderr = stderrStream.toString().trim();
+            evaluator.accept(testStdout, testStderr);
 
         } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         } finally {
-            System.setOut(out);
-            System.out.println(testResults);
+            System.setOut(defaultStdout); // restore stdout/stderr
+            System.setErr(defaultStderr);
+
+            System.err.println(testStderr);
+            System.out.println(testStdout); // print test outputs
         }
+    }
+
+    @Test
+    public void testApplicationWithTwoArguments() {
+        testApplicationWithArguments((out, err) -> {
+            // apparently ".net" will always win since Google will ignore the dot and crush it with 10x results
+            assertTrue(out.contains("Total winner: .net"));
+            // Nothing should be printed to stderr
+            assertEquals("", err);
+        }, "java", ".net");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testLessThan2Arguments() {
-        try {
-            Application.main("nope");
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        testApplicationWithArguments((out, err) -> {}, "nope");
     }
 }
